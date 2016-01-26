2ffa6df0968c88d35362190e0913691ea6ba7710
Boisy Pitre <boisy.pitre@tee-boy.com>
2016-01-26 16:19:28 -0500

    Pushing AffdexEmoji

---
+++ apps/Emoji/Classes/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  faceDetection
+//
+//  Created by Affectiva on 2/22/13.
+//  Copyright (c) 2016 Affectiva Inc.
+//
+//  See the file license.txt for copying permission.
+
+#import <UIKit/UIKit.h>
+
+@class AffdexDemoViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
